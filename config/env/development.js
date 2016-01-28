38682fa7f439469a178fea0c32a3daebb3524f59
ajain27 <a4ankit27@gmail.com>
2016-01-28 13:58:47 -0500

    First MEAN Commit

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: "mongodb://ajain:ajain@ds047865.mongolab.com:47865/mean-database",
     options: {
-      user: '',
-      pass: ''
+      user: 'ajain',
+      pass: 'ajain'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
